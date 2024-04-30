48e473e95ca565c8d03ddba8f0a97f2896521a6b
Sulthan1901 <sulthanzaki1901@gmail.com>
2024-04-30 23:43:31 +0700

    percobaan commit kedua

--- 008.cpp
+++ 008.cpp
@@ -1,63 +1,50 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    cout<<"Masukkan nomor rekening anda : ";          
-    int validasi,valid,validasi2,valid2;    
-    long long norek,mods, dive;             
-    cin>>norek;                             
-    mods=100;                               
-    dive=10;                                
-    validasi=0;
-    do
-    {
-        valid= (((norek%mods)/ dive)*2);    
-        if (valid>=10|| valid <0)           
-            {
-                int angka_1, angka_2;
-                angka_1= valid % 10;
-                angka_2= valid / 10;
-                valid = angka_1+angka_2;
-            } 
-            else
-            {
-                valid= valid;
+int main() {
+    cout << "Enter your account number: ";
+    int validation, valid, validation2, valid2;
+    long long account_number, mods, divide;
+    cin >> account_number;
+    mods = 100;
+    divide = 10;
+    validation = 0;
+    do {
+        valid = (((account_number % mods) / divide) * 2);
+        if (valid >= 10 || valid < 0) {
+            int digit_1, digit_2;
+            digit_1 = valid % 10;
+            digit_2 = valid / 10;
+            valid = digit_1 + digit_2;
+        } else {
+            valid = valid;
+        }
+        validation = validation + valid;
+        mods *= 100;
+        divide *= 100;
+    } while ((mods <= 10000000000000000) || (divide <= 1000000000000000));
 
-            }
-        validasi=validasi +valid;          
-        mods*=100;                          
-    } while ((mods<=10000000000000000)||(dive<=1000000000000000)); 
+    mods = 10;
+    divide = 1;
+    validation2 = 0;
+    do {
+        valid2 = ((account_number % mods) / divide);
+        validation2 = validation2 + valid2;
+        mods *= 100;
+        divide *= 100;
+    } while ((mods <= 1000000000000000) || (divide <= 100000000000000));
 
-    mods=10;
-    dive=1;
-    validasi2=0; 
-    do              
-    {
-        valid2= ((norek%mods)/ dive); 
-        validasi2=validasi2+valid2;     
-        mods*=100;                      
-        dive*=100;
-    } while ((mods<=1000000000000000)||(dive<=100000000000000)); 
+    int total_validation = validation + validation2;
 
-    int valtol= validasi+validasi2; 
-
-    string vld;     
-    if ((((norek%10000000000000000)/1000000000000000)==5)&&(valtol% 10== 0))    
-    {
-        vld="MASTERCARD";
+    string card_type;
+    if ((((account_number % 10000000000000000) / 1000000000000000) == 5) && (total_validation % 10 == 0)) {
+        card_type = "MASTERCARD";
+    } else if ((((account_number % 10000000000000000) / 1000000000000000) == 4) && (total_validation % 10 == 0)) {
+        card_type = "VISA";
+    } else {
+        card_type = "invalid";
     }
-    else if((((norek%10000000000000000)/1000000000000000)==4)&&(valtol% 10==0))
-    {
-        vld="VISA";
-    }
-    else
-    {
-        vld="Nomor rekening idak valid";
-    }
-    cout<<"Nomor kartu anda :"<< norek<<endl; 
-    cout<<"Tipe kartu anda adalah :"<< vld <<endl;
-    cout<<"checksum: "<<valtol<<endl;
-
+    cout << "Your card number: " << account_number << endl;
+    cout << "Your card type: " << card_type << endl;
+    cout << "Checksum: " << total_validation << endl;
     return 0;
-
 }
